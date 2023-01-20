2536d8bba7398af366c624ab06eabc959d6e2e1a
Marina-Nady <marina.youssef197@gmil.com>
2023-01-21 00:09:43 +0200

    add router

--- EcommerceApp/src/app/app.module.ts
+++ EcommerceApp/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { HeaderComponent } from './header/header.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ProductItemComponent,
-    HeaderComponent
+    HeaderComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
