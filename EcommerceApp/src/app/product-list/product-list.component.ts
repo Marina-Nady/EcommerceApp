83595791a5cffdb2bbe14ddbb01977a6862f7175
Marina-Nady <marina.youssef197@gmil.com>
2023-01-20 22:47:31 +0200

    add service

--- EcommerceApp/src/app/product-list/product-list.component.ts
+++ EcommerceApp/src/app/product-list/product-list.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { ProductsService } from '../service/products.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  constructor(private service: ProductsService){}
+  products: any;
+
+  ngOnInit(): void {
+
+    this.service.getProducts().subscribe(
+
+      (response) => {
+        console.log(response)
+        this.products = response;
+      },
+
+      (error) => {
+        console.log(error);
+      }
+    )
+  }
 
 }
---
+++ EcommerceApp/src/app/service/products.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  baseURL = "http://localhost:3000/products";
+
+  getProducts(){
+    return this.httpClient.get(this.baseURL);
+  }
+
+  getUserById(id: number){
+    return this.httpClient.get(`${this.baseURL}/${id}`, { observe: 'response' });
+  }
+
+  updateUserById(id: number, aUser: any) {
+    return this.httpClient.put(`${this.baseURL}/${id}`, aUser);
+  }
+}
