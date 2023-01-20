f2fcd81a485001301fc28db24015d6cb08f13576
Marina-Nady <marina.youssef197@gmil.com>
2023-01-20 21:16:53 +0200

    add headr,product item and product list

--- EcommerceApp/src/app/app.module.ts
+++ EcommerceApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
